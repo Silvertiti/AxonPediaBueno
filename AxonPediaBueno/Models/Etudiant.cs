e9e4bd0e7236eb90e9b8e14b63a43112b7c99e95
Silvertiti <metheotim@gmail.com>
2025-04-08 16:16:47 +0200

    Envoie sur la page

--- AxonPediaBueno/Models/Etudiant.cs
+++ AxonPediaBueno/Models/Etudiant.cs
@@ -1,10 +1,19 @@
-﻿namespace AxonPediaBueno.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AxonPediaBueno.Models
 {
     public class Etudiant
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Le prénom est obligatoire.")]
         public string Prenom { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Le nom est obligatoire.")]
         public string Nom { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "L'email est obligatoire.")]
+        [EmailAddress(ErrorMessage = "L'email n'est pas valide.")]
         public string Email { get; set; } = string.Empty;
     }
 }
